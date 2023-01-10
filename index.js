2c493730090f21a3633cf8358820906108df1d1a
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-10 19:20:09 +0300

    Initial

--- index.js
+++ index.js
@@ -51,20 +51,22 @@ bot.on('message', async (msg) => {
 });
 
 app.post('/web-data', async (req, res) => {
-    const {queryId, products = [], totalPrice} = req.body;
-    try {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Успешная покупка',
-            input_message_content: {
-                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
-            }
-        })
-        return res.status(200).json({});
-    } catch (e) {
-        return res.status(500).json({})
-    }
+    // const {queryId, products = [], totalPrice} = req.body;
+    // try {
+    //     await bot.answerWebAppQuery(queryId, {
+    //         type: 'article',
+    //         id: queryId,
+    //         title: 'Успешная покупка',
+    //         input_message_content: {
+    //             message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+    //         }
+    //     })
+    //     return res.status(200).json({});
+    // } catch (e) {
+    //     return res.status(500).json({})
+    // }
+
+    bot.sendMessage(chatId, '69 >>> Тест')
 })
 
 const PORT = 8000;
