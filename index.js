45802437a4b9d15312becb08643df6b7cffa7329
gorbatkoff <gorbatkovartyom@gmail.com>
2023-01-11 01:59:26 +0300

    Init

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const token = '5833515258:AAHm5gAkLr7XcD3JGiQdOxDRXky3sby6dUw';
 const webAppUrl = 'https://gorgeous-torte-e54231.netlify.app';
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 const app = express();
 
 app.use(express.json());
@@ -15,11 +15,11 @@ bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
-    if(text === '/start') {
+    if (text === '/start') {
         await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
             reply_markup: {
                 keyboard: [
-                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
+                    [{ text: 'Заполнить форму', web_app: { url: webAppUrl + '/form' } }]
                 ]
             }
         })
@@ -27,13 +27,13 @@ bot.on('message', async (msg) => {
         await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
             reply_markup: {
                 inline_keyboard: [
-                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
+                    [{ text: 'Сделать заказ', web_app: { url: webAppUrl } }]
                 ]
             }
         })
     }
 
-    if(msg?.web_app_data?.data) {
+    if (msg?.web_app_data?.data) {
         try {
             const data = JSON.parse(msg?.web_app_data?.data)
             console.log(data)
@@ -43,7 +43,7 @@ bot.on('message', async (msg) => {
 
             setTimeout(async () => {
                 await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
-            }, 3000)
+            }, 5000)
         } catch (e) {
             console.log(e);
         }
@@ -51,22 +51,21 @@ bot.on('message', async (msg) => {
 });
 
 app.post('/web-data', async (req, res) => {
-    // const {queryId, products = [], totalPrice} = req.body;
-    // try {
-    //     await bot.answerWebAppQuery(queryId, {
-    //         type: 'article',
-    //         id: queryId,
-    //         title: 'Успешная покупка',
-    //         input_message_content: {
-    //             message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
-    //         }
-    //     })
-    //     return res.status(200).json({});
-    // } catch (e) {
-    //     return res.status(500).json({})
-    // }
+    const { queryId, products = [], totalPrice } = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
 
-    bot.sendMessage(chatId, '69 >>> Тест')
 })
 
 const PORT = 8000;
